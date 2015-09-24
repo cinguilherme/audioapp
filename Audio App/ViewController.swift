ff608838d38b08327ffa1a5b90ebbdc266dd08f9
Guilherme Carvalho Cintra <cinguilherme@gmail.com>
2015-09-24 16:15:40 -0300

    first commit, simple audio player.

--- Audio App/ViewController.swift
+++ Audio App/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var seekSlider: UISlider!
+    @IBOutlet weak var musciNameLabel: UILabel!
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        player.stop()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        player.stop()
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    
+    
+    @IBAction func volumeUpdate(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    
+    
+    @IBAction func seekUpdate(sender: AnyObject) {
+        //tobedone
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var audioPath = NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")
+        
+        var url:NSURL = NSURL(fileURLWithPath: audioPath!)!
+        
+        var erro:NSError? = nil
+        
+        player = AVAudioPlayer(contentsOfURL: url, error: &erro)
+        
+        if erro == nil {
+            player.prepareToPlay()
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
